c27088be83b8d4dfa174f5e3e24eb1ecfbcd5950
Remco Tolsma <info@remcotolsma.nl>
2015-11-19 11:56:12 +0100

    Extend from common library.

--- src/Integration.php
+++ src/Integration.php
@@ -1,10 +1,26 @@
 <?php
 
-class Pronamic_WP_Pay_Gateways_FortisBank_IDealHosted_Integration {
+class Pronamic_WP_Pay_Gateways_FortisBank_IDealHosted_Integration extends Pronamic_WP_Pay_Gateways_AbstractIntegration {
 	public function __construct() {
 		$this->id         = 'fortis-bank-ideal-hosted';
 		$this->name       = 'Fortis Bank - iDEAL Hosted';
 		$this->provider   = 'fortis-bank';
 		$this->deprecated = true;
+	}
+
+	public function get_config_factory_class() {
+		return null;
+	}
+
+	public function get_config_class() {
+		return null;
+	}
+
+	public function get_settings_class() {
+		return null;
+	}
+
+	public function get_gateway_class() {
+		return null;
 	}
 }
